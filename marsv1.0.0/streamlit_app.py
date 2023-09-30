1c26fd8a4b0f071898992062252faa8141945b5b
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-09-30 07:23:09 +0200

    Update streamlit_app.py

--- marsv1.0.0/streamlit_app.py
+++ marsv1.0.0/streamlit_app.py
@@ -28,7 +28,7 @@ with z:
     st.markdown("![Alt Text](https://i.postimg.cc/05GkJpWY/reg.png)")
 with astep:
     st.markdown("<h4 style='text-align: left; color: darkred;'>Academic Student Tutorial Excellence Programme🧑🏼‍🎓 👨🏽‍🎓</h4>", unsafe_allow_html=True)
-    #st.markdown("<h6 style='text-align: left; color: grey;'>🧑🏼‍🎓 👨🏽‍🎓Supplemental</h6>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: darkred;'>Merging Attendance RegisterS (MARS)</h2>", unsafe_allow_html=True)
     #st.markdown("<h6 style='text-align: left; color: grey;'>Instruction (UFS)👨🏽‍🏫</h6>", unsafe_allow_html=True)    
 with logo:
     #st.markdown('<img src = "app/static/reg.png" style="width:100%">', unsafe_allow_html=True)
