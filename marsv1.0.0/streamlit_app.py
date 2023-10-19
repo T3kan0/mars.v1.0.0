354279b402c03392c408c6b794bd2745ee31c722
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-10-19 21:55:12 +0200

    Update streamlit_app.py

--- marsv1.0.0/streamlit_app.py
+++ marsv1.0.0/streamlit_app.py
@@ -386,11 +386,11 @@ if bulk_files is not None:
                   )
                 st.info(':orange[Well Done!! Ready to Split Rows with Paired Tutors.]', icon="ℹ️")
 
-        with selector:
+        if edits == "rainbow[**Split & Duplicate Row Content**]":
             if len(n_files) == 0:
                 st.write(' ')
             elif len(n_files) >=1:
-                if edits == "rainbow[**Split & Duplicate Row Content**]":
+                with selector:
                     Add_btn = st.button('Split & Duplicate Row')
                     st.write(':orange[This option will separate rows with paired tutors 👉🏾: ]')    
     else:    
