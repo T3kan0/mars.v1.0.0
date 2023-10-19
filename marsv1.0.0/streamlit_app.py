f037af54a55446a5fa47fff4d8121fa52bba8f03
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-10-19 21:51:19 +0200

    Update streamlit_app.py

--- marsv1.0.0/streamlit_app.py
+++ marsv1.0.0/streamlit_app.py
@@ -225,12 +225,7 @@ if bulk_files is not None:
             elif len(n_files) >=1:
                 Add_more = st.button(':red[Add Columns:]')
                 st.write(':orange[This option will add two more columns to the uploaded data files 👉🏾: ]')
-        elif edits == "rainbow[**Split & Duplicate Row Content**]":
-            if len(n_files) == 0:
-                st.write(' ')
-            elif len(n_files) >=1:                
-                Add_btn = st.button('Split & Duplicate Row')
-                st.write(':orange[This option will separate rows with paired tutors 👉🏾: ]')
+
     with adder:
          if edits == ":rainbow[**Add Columns**]":
             if len(n_files) == 0:
@@ -389,7 +384,15 @@ if bulk_files is not None:
                         file_name='new_file.csv',
                         mime="file/csv"
                   )
-                st.info(':orange[Well Done!! Ready to Split Rows with Paired Tutors.]', icon="ℹ️")                         
+                st.info(':orange[Well Done!! Ready to Split Rows with Paired Tutors.]', icon="ℹ️")
+
+        with selector:
+            if len(n_files) == 0:
+                st.write(' ')
+        elif len(n_files) >=1:
+            if edits == "rainbow[**Split & Duplicate Row Content**]":
+                Add_btn = st.button('Split & Duplicate Row')
+                st.write(':orange[This option will separate rows with paired tutors 👉🏾: ]')    
     else:    
         st.info(':red[ 🚩 Remember to Upload Your Files] 🚩', icon="ℹ️")
         
