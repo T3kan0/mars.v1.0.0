74e8194b7e279c214fb78e9e2a767afb3621a902
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-10-23 19:00:57 +0200

    Update streamlit_app.py

--- marsv1.0.0/streamlit_app.py
+++ marsv1.0.0/streamlit_app.py
@@ -334,18 +334,18 @@ if bulk_files is not None:
                 st.success('Files Successfully Merged!', icon="✅")               
     
         if aggreg:
-            with open('bulk_file.csv', "rb") as file:
-                # Convert to Excel (in memory)
-                excel_buffer = io.BytesIO()
-                with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
-                    new_file.to_excel(writer, index=False, sheet_name='Aggregated_Data')
-                excel_data = excel_buffer.getvalue()
-                btn = st.download_button(
+            #with open('bulk_file.csv', "rb") as file:
+            # Convert to Excel (in memory)
+            excel_buffer = io.BytesIO()
+            with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
+                new_file.to_excel(writer, index=False, sheet_name='Aggregated_Data')
+            excel_data = excel_buffer.getvalue()
+            btn = st.download_button(
                     label=":red[Download Aggregated File]",
                     data=excel_data,
                     file_name='Bulk_File_MARS.xlsx',
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-              )
+            )
         else:
             st.write(':blue[Press Aggregate Files to Continue]')
             
