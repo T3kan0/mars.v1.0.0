d08fa3c0e728771faf426dc2e2f8e96f02b618b4
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-10-27 08:45:09 +0200

    Update streamlit_app.py

--- marsv1.0.0/streamlit_app.py
+++ marsv1.0.0/streamlit_app.py
@@ -309,7 +309,6 @@ if bulk_files is not None:
     if edits == ":rainbow[**Remove Columns**]":
         new_files = st.session_state.get("new_file", None)
         #st.session_state.aggre_files = new_file  # store in session
-        st.dataframe(new_files)
         
         with selector:
             if len(n_files) == 0:
